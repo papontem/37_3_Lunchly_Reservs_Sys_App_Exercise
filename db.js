9de67b4f19230d90d7774baf5d79872da692a7e3
papontemjetix <papontem@gmail.com>
2023-09-04 15:49:06 -0400

    updated db.js , server.js, and ran npm update --save to update packages.

--- db.js
+++ db.js
@@ -2,8 +2,18 @@
 
 const pg = require("pg");
 
-const db = new pg.Client("postgresql:///lunchly");
+const username = process.env.PG_USERNAME;
+const password = process.env.PG_PASSWORD;
+const port = process.env.PGPORT;
 
+let db_name = process.env.NODE_ENV === "test" ? "lunchly_test" : "lunchly";
+
+const db = new Client({
+	host: "localhost",
+	user: username,
+	port: port,
+	password: password,
+	database: db_name,
+});
 db.connect();
-
 module.exports = db;
